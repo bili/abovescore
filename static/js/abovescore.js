bbde1a939d83b7f8acf56d5baaee15612e42e0ce
bili <panning20@gmail.com>
2015-11-05 16:29:50 +0800

    Put DEBUG switcher into function

--- static/js/abovescore.js
+++ static/js/abovescore.js
@@ -113,7 +113,7 @@
     };
 	
 	_.log = function() {
-		if (!_.isUndefined(this.DEBUG) && this.DEBUG == true) console && console.log.apply(console, arguments);
+		if (!_.isUndefined(_.log.DEBUG) && _.log.DEBUG == true) console && console.log.apply(console, arguments);
 	};
 		
     var root = typeof exports !== "undefined" && exports !== null ? exports : window;
