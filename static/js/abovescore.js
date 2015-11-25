67f4073af6be6a5461735d829247b4656650f877
bili <panning20@gmail.com>
2015-11-25 11:10:59 +0800

    add css support detection

--- static/js/abovescore.js
+++ static/js/abovescore.js
@@ -115,7 +115,31 @@
 	_.log = function() {
 		if (!_.isUndefined(_.log.DEBUG) && _.log.DEBUG == true) console && console.log.apply(console, arguments);
 	};
-		
+	
+    _.support = (function() {
+        var div = document.createElement('div'),
+        vendors = 'Khtml Ms O Moz Webkit'.split(' '),
+        len = vendors.length;
+
+        // prop: css name
+        return function(prop) {
+            if ( prop in div.style ) return true;
+
+            prop = prop.replace(/^[a-z]/, function(val) {
+                return val.toUpperCase();
+            });
+
+            while(len--) {
+                if ( vendors[len] + prop in div.style ) {
+                    // browser supports box-shadow. Do what you need.
+                    // Or use a bang (!) to test if the browser doesn't.
+                    return true;
+                } 
+            }
+            return false;
+        };
+ 	})();
+    
     var root = typeof exports !== "undefined" && exports !== null ? exports : window;
     root._ = _;
 
--- static/js/index.js
+++ static/js/index.js
@@ -2,3 +2,4 @@ console.log(_.type('fasfsd'), _.type(234), _.type(new Date), _.type(false));
 console.log(_.isNumber('342'), _.isNumber(2342));
 var bili = {name: 'bili', age: 27};
 console.log(_.tmpl("i'm #{name}, a #{age} year-old boy.", bili));
+console.log(_.support('animation'));
